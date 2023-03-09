aa21ed7a216b5480695153412747b30a32880543
João Vitor Espig <jotinha1300@gmail.com>
2023-03-08 23:08:20 -0300

    added the firsts stages of code

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3.8
 
+# Explanation: https://en.wikipedia.org/wiki/Turing_machine
+
+STATES = {'right', 'carry', 'done'}
+TAPE_ALPHABET_SYMBOLS = {'', '0', '1'}
+BLANK_SYMBOL = ''
+INPUT_SYMBOLS = {'0', '1'}
+INITIAL_STATE = 'right'
+FINAL_STATE = 'done'
+
+# Move tape
+R = 0
+L = 1
+
+
+def action(write_symbol: str, move_tape: int, next_state: str) -> None:
+    pass
+
+
+def add_one(input: str) -> None:
+    pass
+
+
 def main() -> None:
     print("hello")
 
